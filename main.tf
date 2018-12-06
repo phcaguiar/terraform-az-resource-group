f7d5aebf73231fb214bfa63e42d8ee1ef9c628fa
Pedro Aguiar <pedro_deaguiar@yahoo.com.br>
2018-12-06 03:26:13 +0000

    change resource and variables name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-resource "azurerm_resource_group" "resource_group" {
-  name 		  =   "${var.resource_group_name}"
-  location  =   "${var.location}"
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}"
+  location = "${var.location}"
   tags {
-    team    =   "${var.tag_team}"
-  }
+    team   = "${var.tag_team}"
+  }  
 }
